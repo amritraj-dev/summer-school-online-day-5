a8292a4a3b73f2e89844076a551e9e53ee65f690
Amrit Raj <ramrit97@gmail.com>
2025-07-09 16:38:15 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ function success(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const apiKey = "af52447517010ffc56e95f8b46a1313c"; 
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=chandigarh'
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=chandigarh';
 
     fetch(url)
     .then(response => {
